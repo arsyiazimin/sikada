c1cc03c0cd9ae9b422dd0b9f78282a1cb06dbd77
Muh Arsyi Azimin <arsyiazimin@gmail.com>
2020-08-21 18:03:49 +0700

    change prod

--- config/production.ts
+++ config/production.ts
@@ -1,27 +1,27 @@
 export default {
-  HOST: 'https://localhost', // dev
-  PORT: '3500', // dev
+  HOST: 'http://127.0.0.1', // dev
+  PORT: '3000', // dev
   SECRET: 'asdasf;qqwrlmsdfmafk;rqwelqkrjlsdffsdfdsjhfksrweriuosfpyuirewbfmsdfbmsdfiwher;dsf;rerkhjgkertkr',
   JWT_EXPIRED: 604800,
   DAILY_EXPIRED: 86400,
   SERVER_LINK: '',
   email: {
-    USE_EMAIL_TESTER: 'no', //dev
+    USE_EMAIL_TESTER: 'yes', //dev
     EMAIL_TO_TESTER: 'arsyiazimin@gmail.com'
   },
   db: {
     DB_TYPE: 'mysql',
-    DB_HOST: 'localhost',
+    DB_HOST: '5.181.216.118',
     DB_PORT: 3306,
-    DB_USER_NAME: 'root',
-    DB_PASSWORD: '',
-    DB_NAME: 'db_login',
+    DB_USER_NAME: 'u1113286_root',
+    DB_PASSWORD: 'sikadasuperuser',
+    DB_NAME: 'u1113286_sikada',
     DB_SYNCHRONIZE: false,
     DB_LOGGING: true
   },
   microservices: {
     TYPE: 'redis',
-    HOST: '',
+    HOST: '192.168.56.101',
     PORT: 6379,
     DURATION: 60000
   }
