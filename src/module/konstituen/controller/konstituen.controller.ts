ff2193e0050541be2a12c1f38d7b17626df4cbb2
Muh Arsyi Azimin <arsyiazimin@gmail.com>
2020-08-21 20:23:02 +0700

    v12

--- src/module/konstituen/controller/konstituen.controller.ts
+++ src/module/konstituen/controller/konstituen.controller.ts
@@ -29,7 +29,7 @@ export class KonstituenController {
         return this.konstituenService.uploadData(file, res)
     }
 
-    @Get('getalldata')
+    @Get('getAllData')
     async getAllData(@Res() res) {
         return await this.konstituenService.getAllData(res)
     }
